b4695a2b08600d6cb51d5ad10981dc7acdbc0adc
Tejashree06-git <tejashrideshpande66@gmail.com>
2020-09-14 15:24:03 +0530

    Added HTTP services for details page

--- src/app/moviedetails/moviedetails.page.ts
+++ src/app/moviedetails/moviedetails.page.ts
@@ -9,18 +9,20 @@ import { AppComponent } from '../app.component';
   styleUrls: ['./moviedetails.page.scss'],
 })
 export class MoviedetailsPage implements OnInit {
-
+movieTitle = "";
   constructor(private router:Router,private http:HttpClient,private appComp:AppComponent) { }
 
   ngOnInit() {
+    this.displayMovieDetails();
+
   }
 displayMovieDetails()
 {
   let movieID = localStorage.getItem('movieID');
-  let url = this.appComp.apiUrl;
+  let url = this.appComp.apiUrl+"?imdbID="+movieID;
   this.http.get(url).subscribe(data => {
     console.log(data);
-
+    this.movieTitle = data[0].Title;
   },
   err => {
   console.log(err);
